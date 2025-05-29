c989df3ce4457c98f59ee7a9b0e2b3c679e317dc
Tristan Zatylny <tristanzatylny@gmail.com>
2025-05-29 11:09:58 -0500

    try #3 i hope this works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'dad';
+  return <div>Hi {value}</div>;
 }
 
 export default App;
